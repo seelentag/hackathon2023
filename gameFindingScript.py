856afc9831bd0f24146ffac36fe9398aefa0bc81
josephmdugan <110262365+josephmdugan@users.noreply.github.com>
2023-04-15 12:34:41 -0700

    Changed Comments

--- gameFindingScript.py
+++ gameFindingScript.py
@@ -1,7 +1,29 @@
 
-# DnD Game Finding Script
+# TTRPG Game Finding Script
 # Cayden Mcdonough, Joey Dugan
 
 # Written 4/15/23 for the Cal Poly Humboldt Hackathon
+
+
+
+
+# Poking through initial CSV file with information 
+
+
+
+# Creating a dictionary of people with inner lists of avaible times and other preferences
+
+
+
+
+# Matching people based on their available times
+
+
+
+
+# Outputing a CSV file with grouped users
+
+
+
 
 
